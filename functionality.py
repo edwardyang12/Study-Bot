12832b97fc10e30a8408471980c0ecc821bd46ff
Hana Kim <hsk003@ucsd.edu>
2019-11-10 21:24:10 -0800

    accounts for background noise

--- functionality.py
+++ functionality.py
@@ -1,6 +1,7 @@
 import pyaudio,os
 import speech_recognition as sr
 def mainfunction(source):
+    r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
     user = r.recognize_google(audio)
     print(user)
