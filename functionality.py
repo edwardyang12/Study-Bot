6d9dcd440e692c0ccf9f0e7effdd68d2576911b9
Emily Jewik <emilyjewik@gmail.com>
2019-11-10 23:25:56 -0800

    Added the timer function

--- functionality.py
+++ functionality.py
@@ -1,11 +1,27 @@
 import pyaudio,os
 import speech_recognition as sr
+import time
+import datetime
 def mainfunction(source):
     audio = r.listen(source)
-    user = r.recognize_google(audio)
-    print(user)
-    if user == "hello":
-        print("you said hello")
+    my_time  = r.recognize_google(audio)
+    
+    #Possible audio input strings
+    timeStrings = ["one", "two", "three", "four", "five"]
+    timeIntegerStrings = ["1", "2", "3", "4", "5"]
+
+    #Times for timer as ints
+    sleepTime = [1, 2, 3, 4, 5]
+    counter = 0;
+
+    #Sleep for the time specified
+    while counter < len(timeStrings):
+        if str(my_time) == timeStrings[counter] or str( my_time) == timeIntegerStrings[counter]:            
+            print("Sleeping for " + timeStrings[counter] + " seconds")
+            time.sleep(sleepTime[counter])
+        counter = counter + 1    
+    
+    print("Done sleeping")
 
 if __name__ == "__main__":
     r = sr.Recognizer()
