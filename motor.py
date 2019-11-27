961b9782b41e9e2e7b1433e74369bd34c7d77e1d
unknown <edwardyang016@gmail.com>
2019-11-26 16:12:32 -0800

    finished text and motor

---
+++ motor.py
@@ -0,0 +1,59 @@
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+
+Motor2A = 23
+Motor2B = 21
+Motor2E = 19
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
+
+def forwardMove(time):
+    print("Forward")
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.HIGH)
+    sleep(time)
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+
+def backwardMove(time):
+    print("Backward")
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2E,GPIO.HIGH)
+    sleep(time)
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+
+def rotate90(time):
+    print("90 right")
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2E,GPIO.HIGH)
+    sleep(time)
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+
+
+#GPIO.cleanup()
--- text2number.py
+++ text2number.py
@@ -1,14 +1,27 @@
-word = "two"
+word = "eighty two"
 
 def magic(word):
     try:
         intN = int(word)
         return intN
     except:
-        bigList = ["zero","one","two","three","four","five","six","seven", "eight", "nine","ten"]
+        temp = 0
+        found = False
+        bigList = ["zero","one","two","three","four","five","six","seven", "eight", "nine","ten",
+                   "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+        tensList = ["twenty" , "thirty", "forty", "fifty", "sixty"]
+        for i in range(len(tensList)):
+            if(word[:len(tensList[i])] == tensList[i]):
+                temp = temp + (i+2)*10
+                word = word[len(tensList[i])+1:]
+                found = True
         for i in range(len(bigList)):
-            if(bigList[i]==word):
-                return i
-        return "number not found"
+            if(bigList[i] == word):
+                temp = temp + i
+                found = True
+        if(found):
+            return temp
+        else:
+            return "number not found"
 
-#print(type(magic(word)))
+print(magic(word))
